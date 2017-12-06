4dfd85caaa6463c5601a1970ddd63ab72d391b26
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-07 05:33:08 +0800

    Update settings.py

--- blog_t/settings.py
+++ blog_t/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '+79#gfdn6^rj8+35ljogh)gm-#@djoo1fx(1qj*+r^x9@eltfq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [“*”]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
