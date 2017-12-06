403b9a190dd3bbf25e07cef73b9692efafa76b89
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-07 05:45:07 +0800

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from blog.models import Article
+from blog.models import Jun
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(Jun)
