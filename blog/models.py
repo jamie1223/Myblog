7d11d8e8022179e174055d4c6b979fc40408d279
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-07 22:06:33 +0800

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,38 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Model):
+class Article(models.Model):#用来存储文章
     title = models.CharField(max_length=20)
     index = models.CharField(max_length=120,null=True)
     content = models.TextField()
-    create_datatime = models.DateTimeField(auto_now_add=True,null=True)
-    create_data = models.DateField(auto_now_add=True,null=True)
+    create_datatime = models.DateTimeField(null=True)
+    create_data = models.DateField(null=True)
+    pic_link = models.CharField(max_length=500,null=True)
+    top = models.BooleanField(default=False)
 
-
-class Jun(models.Model):
+class Jun(models.Model):#用来存储与君说
     content = models.TextField(max_length=400)
-    create_datatime = models.DateTimeField(auto_now_add=True)
-    create_data = models.DateField(auto_now_add=True)
+    create_datatime = models.DateTimeField()
+    create_data = models.DateField()
 
-class Dream(models.Model):
+class Dream(models.Model):#用来存储梦境
     content = models.TextField(max_length=400)
-    create_datatime = models.DateTimeField(auto_now_add=True)
-    create_data = models.DateField(auto_now_add=True)
+    create_datatime = models.DateTimeField()
+    create_data = models.DateField()
+
+class Author(models.Model):#用来统一保存作者，以防后续需要改名。
+    name = models.CharField(max_length=12)
+
+class Footer(models.Model):#设置页脚的文字
+    content = models.CharField(max_length=120,null=True)
+
+class Tag(models.Model):
+    content = models.CharField(max_length=8)
+
+class Words(models.Model):
+    content = models.CharField(max_length=50)
+
+class Profile(models.Model):
+    state = models.CharField(max_length=40)
+    photo = models.CharField(max_length=500,null=True)
+
