bc2c9ebd9a3d2ef465be9bfc27bf528b1c92eefd
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-07 22:07:54 +0800

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -5,37 +5,55 @@ from django.utils.safestring import mark_safe
 
 def index(request):
     article_list = models.Article.objects.all().order_by("-id")
+    author = models.Author.objects.all().first()
+    footer = models.Footer.objects.all().first()
+    top = models.Article.objects.filter(top=True)
+    tags = models.Tag.objects.all()
+    profile = models.Profile.objects.all().first()
+    words = models.Words.objects.all()
 
-    return render(request,'index.html',{'a_list':article_list})
+    res = {'a_list':article_list,
+            'author':author,
+            'footer':footer,
+            'top':top,
+            'tags':tags,
+            'words':words,
+            'profile':profile}
 
-def test(request):
-    if request.method == 'GET':
-        article_list = models.Article.objects.all()
-        print(article_list)
-        return render(request, 'test.html', {'a_list':article_list})
+    return render(request,'index.html',res)
+
 
 def article(request):
     article_list = models.Article.objects.all().order_by("-id")
+    author = models.Author.objects.all().first()
+    footer = models.Footer.objects.all().first()
 
-    return render(request, 'article.html', {'a_list': article_list})
+    return render(request, 'article.html', {'a_list': article_list,'author':author,'footer':footer})
 
 def dream(request):
-    article_list = models.Article.objects.all().order_by("-id")
+    dream_list = models.Dream.objects.all().order_by("-id")
+    author = models.Author.objects.all().first()
+    footer = models.Footer.objects.all().first()
 
-    return render(request, 'dream.html', {'a_list': article_list})
+    return render(request, 'dream.html', {'d_list': dream_list,'author':author,'footer':footer})
 
 def tojun(request):
     mood_list = models.Jun.objects.all().order_by("-id")
+    author = models.Author.objects.all().first()
+    footer = models.Footer.objects.all().first()
 
-    return render(request, 'tojun.html', {'m_list': mood_list})
+    return render(request, 'tojun.html', {'m_list': mood_list,'author':author,'footer':footer})
 
 def article_content(request):
 
     aid = request.GET.get('aid')
     article = models.Article.objects.filter(id=aid).first()
-    temp = article.content
+    temp = article.content#获取文章内容
+    content = temp.replace(" ","&nbsp;").replace("\r","<br/>")#对文本内容进行处理，使符合html的规则。
+    author = models.Author.objects.all().first()
+    footer = models.Footer.objects.all().first()
+    profile = models.Profile.objects.all().first()
 
-    content = temp.replace(" ","&nbsp;").replace("\r","<br/>")
 
-    return render(request, 'article_content.html', {'article':article,'content':content})
+    return render(request, 'article_content.html', {'article':article,'content':content,'author':author,'footer':footer,'profile':profile})
 
