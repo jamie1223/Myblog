7300ecdc3efa166cae4970e4b687701ddc652aaf
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-08 11:18:49 +0800

    Update settings.py

--- blog_t/settings.py
+++ blog_t/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+79#gfdn6^rj8+35ljogh)gm-#@djoo1fx(1qj*+r^x9@eltfq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -130,4 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_all')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_all')
