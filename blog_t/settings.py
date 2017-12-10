2f7b6134ec0fb9750a1afe851801db2be74323b7
jamie1223 <34306264+jamie1223@users.noreply.github.com>
2017-12-10 17:08:26 +0800

    Update settings.py

--- blog_t/settings.py
+++ blog_t/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+79#gfdn6^rj8+35ljogh)gm-#@djoo1fx(1qj*+r^x9@eltfq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
